b02f47af548b26b3bf8348bc9a34adc9fb798278
Mikael Vinicius <viniciusmikael943@gmail.com>
2023-08-25 10:54:54 -0300

    Initial commit

---
+++ src/src/application/Principal.java
@@ -0,0 +1,11 @@
+package application;
+
+import boardgame.Position;
+
+public class Principal {
+    public static void main(String[] args) {
+        Position position = new Position(3,5);
+
+        System.out.println(position);
+    }
+}
